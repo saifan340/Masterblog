fab427e10a0439e2c4bbd37378a283f14d161206
Saifan Aremenak <saifanaremenak@gmail.com>
2025-07-02 10:49:21 +0200

    json file added, app.py modified

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask ,render_template
+# In-memory list of blog posts
+
 
 app = Flask(__name__)
 
@@ -6,7 +8,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+from flask import
 
+@app.route('/')
+def index():
+    blog_post = random.choice(blog_posts)
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=4999, debug=True)
