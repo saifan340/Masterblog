f36f26dfeb64f1fb530a695ff6dd2b90b32c55ac
Saifan Aremenak <saifanaremenak@gmail.com>
2025-07-02 15:35:44 +0200

    new route and funcztion created ,index.html modified

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 
@@ -10,6 +10,12 @@ def load_posts():
         blog_posts = json.load(fileobj)
     return blog_posts
 
+def save_blog_posts(posts):
+    """Updates the json file"""
+    with open('blog_posts.json', 'w') as fileobj:
+        json.dump(posts, fileobj)
+
+
 @app.route('/greeting')
 def hello_world():
     return 'Hello, World!'
@@ -18,6 +24,28 @@ def hello_world():
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+
+    if request.method == 'POST':
+       author=request.form.get('author')
+       title=request.form.get('title')
+       content=request.form.get('content')
+
+       blog_posts=load_posts()
+       new_post={
+       'id' : len(blog_posts)+1,
+       'author': author,
+       'title': title,
+       'content': content,
+       }
+       blog_posts.append(new_post)
+       save_blog_posts(blog_posts)
+
+       return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=4999, debug=True)
