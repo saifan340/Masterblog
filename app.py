3f0e986d0ca14e41aa573386cd5b8d9a79aec864
Saifan Aremenak <saifanaremenak@gmail.com>
2025-07-02 11:22:51 +0200

    added new files,index route added to app.py

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask ,render_template
-# In-memory list of blog posts
+import json
 
 
 app = Flask(__name__)
 
+def load_posts():
+    """Opens the json file and loads blog posts"""
+    with open("blog_posts.json", "r") as fileobj:
+        blog_posts = json.load(fileobj)
+    return blog_posts
 
-@app.route('/')
+@app.route('/greeting')
 def hello_world():
     return 'Hello, World!'
-from flask import
 
 @app.route('/')
 def index():
-    blog_post = random.choice(blog_posts)
+    blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
